d5d8da2d269999ee8fdf1ae0eb8abb972cda09f2
B3 <b3sigma@gmail.com>
2014-05-16 22:06:11 -1000

    Ended up formatting some files during browsing.

--- CommonRender/Render/Render_XmlSceneLoader.h
+++ CommonRender/Render/Render_XmlSceneLoader.h
@@ -12,58 +12,57 @@ using namespace OVR::Render;
 
 #include "../OculusSDK/3rdParty/TinyXml/tinyxml2.h"
 
-namespace OVR { namespace Render {
+namespace OVR {
+namespace Render {
 
 using namespace tinyxml2;
 
-class XmlHandler
-{
+class XmlHandler {
 public:
-    XmlHandler();
-    ~XmlHandler();
+  XmlHandler();
+  ~XmlHandler();
 
-    bool ReadFile(const char* fileName, OVR::Render::RenderDevice* pRender,
-                  OVR::Render::Scene* pScene,
-		          OVR::Array<Ptr<CollisionModel> >* pColisions,
-                  OVR::Array<Ptr<CollisionModel> >* pGroundCollisions);
+  bool ReadFile(const char* fileName, OVR::Render::RenderDevice* pRender,
+      OVR::Render::Scene* pScene, OVR::Array<Ptr<CollisionModel> >* pColisions,
+      OVR::Array<Ptr<CollisionModel> >* pGroundCollisions);
 
 protected:
-    void ParseVectorString(const char* str, OVR::Array<OVR::Vector3f> *array,
-		                   bool is2element = false);
+  void ParseVectorString(const char* str, OVR::Array<OVR::Vector3f> *array,
+      bool is2element = false);
 
 private:
-    tinyxml2::XMLDocument* pXmlDocument;
-    char                   filePath[250];
-    int                    textureCount;
-    OVR::Array<Ptr<Texture> > Textures;
-    int                    modelCount;
-    OVR::Array<Ptr<Model> > Models;
-    int                    collisionModelCount;
-    int                    groundCollisionModelCount;
+  tinyxml2::XMLDocument* pXmlDocument;
+  char filePath[250];
+  int textureCount;
+  OVR::Array<Ptr<Texture> > Textures;
+  int modelCount;
+  OVR::Array<Ptr<Model> > Models;
+  int collisionModelCount;
+  int groundCollisionModelCount;
 };
 
-}} // OVR::Render
+}
+} // OVR::Render
 
 #ifdef OVR_DEFINE_NEW
 #define new OVR_DEFINE_NEW
 #endif
 
 #endif // INC_Render_XMLSceneLoader_h
-
 /************************************************************************************
-Modified from:
-Copyright   :   Copyright 2013 Oculus VR, Inc. All Rights reserved.
+ Modified from:
+ Copyright   :   Copyright 2013 Oculus VR, Inc. All Rights reserved.
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
 
-http://www.apache.org/licenses/LICENSE-2.0
+ http://www.apache.org/licenses/LICENSE-2.0
 
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
 
-************************************************************************************/
+ ************************************************************************************/
